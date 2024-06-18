97943e1e926bd3b8b2a733ce6e7d484db923e13e
Amine Laghlabi <amine.laghlabi@gmail.com>
2024-06-19 00:50:48 +0100

    Ajout de client avec proche

--- Infrastructure/ClientRepository.js
+++ Infrastructure/ClientRepository.js
@@ -29,17 +29,36 @@ function CreateClient(data) {
             .execute('ps_create_client')
             .then((result) => resolve(result.rowsAffected[0] > 0))
             .catch((error) => reject(error?.originalError?.info?.message))
-
-        // new sql.Request()
-        //     .input("CabinetId", sql.UniqueIdentifier, CabinetId)
-        //     .execute('ps_get_clients')
-        //     .then((result) => {
-        //         resolve(result.recordset)
-        //     })
-        //     .catch((error) => {
-        //         reject(error?.originalError?.info?.message);
-        //     })
     });
 }
-
-module.exports = { GetClients, CreateClient }; // Export the connect function
+function UpdateClient(data) {
+    return new Promise((resolve, reject) => {
+        new sql.Request()
+            .input("ClientId", sql.UniqueIdentifier, data.ClientId)
+            .input("Nom", sql.NVarChar(255), data.Nom)
+            .input("Prenom", sql.NVarChar(255), data.Prenom)
+            .input("DateNaissance", sql.Date, data.DateNaissance)
+            .input("Profession", sql.NVarChar(255), data.Profession)
+            .input("DateRetraite", sql.Date, data.DateRetraite)
+            .input("NumeroSS", sql.NVarChar(20), data.NumeroSS)
+            .input("Adresse", sql.NVarChar(255), data.Adresse)
+            .input("Email1", sql.NVarChar(100), data.Email1)
+            .input("Email2", sql.NVarChar(100), data.Email2)
+            .input("Telephone1", sql.NVarChar(20), data.Telephone1)
+            .input("Telephone2", sql.NVarChar(20), data.Telephone2)
+            .input("HasConjoint", sql.Bit, data.HasConjoint)
+            .execute('ps_update_client')
+            .then((result) => resolve(result.rowsAffected[0] > 0))
+            .catch((error) => reject(error?.originalError?.info?.message))
+    });
+}
+function DeleteClient(ClientId) {
+    return new Promise((resolve, reject) => {
+        new sql.Request()
+            .input("ClientId", sql.UniqueIdentifier, ClientId)
+            .execute('ps_delete_client')
+            .then((result) => resolve(result.rowsAffected[0] > 0))
+            .catch((error) => reject(error?.originalError?.info?.message))
+    });
+}
+module.exports = { GetClients, CreateClient, UpdateClient, DeleteClient };
--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const sql = require("mssql");
 // const sql = require("mssql");
 const { log } = require('console');
 const { connect } = require('./db');
-const { GetClients, CreateClient } = require('./repository');
+const { GetClients, CreateClient, UpdateClient, DeleteClient } = require('./Infrastructure/ClientRepository');
+const { GetProches, CreateProche, UpdateProche, DeleteProche } = require('./Infrastructure/ProcheRepository');
 require('./ClientController');
 const app = express();
 const cors = require('cors');
@@ -80,133 +81,56 @@ app.get("/GetClients", async (request, response) => {
     .then((res) => response.status(200).send(res))
     .catch((error) => response.status(400).send(error))
 });
-
 app.post("/CreateClient", async (request, response) => {
   await CreateClient(request.body)
+    .then(async (res) => {
+      if (res != null && res == true)
+        if (request.body.Proches != null && request.body.Proches.length > 0) {
+          // create proches
+          for (let i = 0; i < request.body.Proches.length; i++) {
+            await CreateProche(request.body.Proches[i])
+              .then((resProche) => {
+                console.log("resProche: ", resProche)
+              })
+              .catch((errorProche) => { console.log("ErrorProche: ", errorProche); })
+          }
+        }
+      response.status(200).send(res);
+    })
+    .catch((error) => response.status(400).send(error))
+});
+app.put("/UpdateClient", async (request, response) => {
+  await UpdateClient(request.body)
     .then((res) => response.status(200).send(res))
     .catch((error) => response.status(400).send(error))
 });
-
-app.put("/UpdateClient", (request, response) => {
-  // console.log("/CreateClient");
-  new sql.Request()
-    .input("ClientId", sql.UniqueIdentifier, request.body.ClientId)
-    .input("Nom", sql.NVarChar(255), request.body.Nom)
-    .input("Prenom", sql.NVarChar(255), request.body.Prenom)
-    .input("DateNaissance", sql.Date, request.body.DateNaissance)
-    .input("Profession", sql.NVarChar(255), request.body.Profession)
-    .input("DateRetraite", sql.Date, request.body.DateRetraite)
-    .input("NumeroSS", sql.NVarChar(20), request.body.NumeroSS)
-    .input("Adresse", sql.NVarChar(255), request.body.Adresse)
-    .input("Email1", sql.NVarChar(100), request.body.Email1)
-    .input("Email2", sql.NVarChar(100), request.body.Email2)
-    .input("Telephone1", sql.NVarChar(20), request.body.Telephone1)
-    .input("Telephone2", sql.NVarChar(20), request.body.Telephone2)
-    .input("HasConjoint", sql.Bit, request.body.HasConjoint)
-    .execute('ps_update_client')
-    .then((result) => {
-      response.status(200).send(result.rowsAffected[0] > 0);
-    })
-    .catch((error) => {
-      response.status(400).send(error?.originalError?.info?.message);
-    })
-});
-
-app.delete("/DeleteClient/:ClientId", (request, response) => {
-  new sql.Request()
-    .input("ClientId", sql.UniqueIdentifier, request.params.ClientId)
-    .execute('ps_delete_client')
-    .then((result) => {
-      response.status(200).send(result.rowsAffected[0] > 0);
-    })
-    .catch((error) => {
-      console.log(error)
-      response.status(400).send(error?.originalError?.info?.message);
-    })
+app.delete("/DeleteClient/:ClientId", async (request, response) => {
+  await DeleteClient(request.params.ClientId)
+    .then((res) => response.status(200).send(res))
+    .catch((error) => response.status(400).send(error))
 });
 //#endregion Client
 
 //#region Proche
-app.get("/GetProches", (request, response) => {
-  new sql.Request()
-    .input("ClientId", sql.UniqueIdentifier, request.query.ClientId)
-    .execute('ps_get_proches')
-    .then((result) => {
-      setTimeout(() => {
-        response.status(200).send(result.recordset);
-      }, 1000);
-    })
-    .catch((error) => {
-      response.status(400).send(error?.originalError?.info?.message);
-    })
+app.get("/GetProches", async (request, response) => {
+  await GetClients(request.query.ClientId)
+    .then((res) => response.status(200).send(res))
+    .catch((error) => response.status(400).send(error))
 });
-
-app.post("/CreateProche", (request, response) => {
-  // console.log("/CreateProche");
-  new sql.Request()
-    .input("ProcheId", sql.UniqueIdentifier, request.body.ProcheId)
-    .input("ClientId", sql.UniqueIdentifier, request.body.ClientId)
-    .input("Nom", sql.NVarChar(100), request.body.Nom)
-    .input("Prenom", sql.NVarChar(100), request.body.Prenom)
-    .input("DateNaissance", sql.Date, request.body.DateNaissance)
-    .input("Telephone1", sql.NVarChar(20), request.body.Telephone1)
-    .input("Telephone2", sql.NVarChar(20), request.body.Telephone2)
-    .input("Email1", sql.NVarChar(100), request.body.Email1)
-    .input("Email2", sql.NVarChar(100), request.body.Email2)
-    .input("Adresse", sql.NVarChar(255), request.body.Adresse)
-    .input("Charge", sql.Bit, request.body.Charge)
-    .input("LienParente", sql.NVarChar(100), request.body.LienParente)
-    .input("Particularite", sql.NVarChar(100), request.body.Particularite)
-    .input("NombreEnfant", sql.NVarChar(255), request.body.NombreEnfant)
-    .input("Commentaire", sql.NVarChar(255), request.body.Commentaire)
-    .execute('ps_create_Proche')
-    .then((result) => {
-      response.status(200).send(result.rowsAffected[0] > 0);
-    })
-    .catch((error) => {
-      console.log(error?.originalError)
-      console.log(error?.originalError?.info)
-      response.status(400).send(error?.originalError?.info?.message);
-    })
+app.post("/CreateProche", async (request, response) => {
+  await CreateProche(request.body)
+    .then((res) => response.status(200).send(res))
+    .catch((error) => response.status(400).send(error))
 });
-
-app.put("/UpdateProche", (request, response) => {
-  // console.log("/CreateProche");
-  new sql.Request()
-    .input("ProcheId", sql.UniqueIdentifier, request.body.ProcheId)
-    .input("Nom", sql.NVarChar(100), request.body.Nom)
-    .input("Prenom", sql.NVarChar(100), request.body.Prenom)
-    .input("DateNaissance", sql.Date, request.body.DateNaissance)
-    .input("Telephone1", sql.NVarChar(20), request.body.Telephone1)
-    .input("Telephone2", sql.NVarChar(20), request.body.Telephone2)
-    .input("Email1", sql.NVarChar(100), request.body.Email1)
-    .input("Email2", sql.NVarChar(100), request.body.Email2)
-    .input("Adresse", sql.NVarChar(255), request.body.Adresse)
-    .input("Charge", sql.Bit, request.body.Charge)
-    .input("LienParente", sql.NVarChar(100), request.body.LienParente)
-    .input("Particularite", sql.NVarChar(100), request.body.Particularite)
-    .input("NombreEnfant", sql.NVarChar(255), request.body.NombreEnfant)
-    .input("Commentaire", sql.NVarChar(255), request.body.Commentaire)
-    .execute('ps_update_proche')
-    .then((result) => {
-      response.status(200).send(result.rowsAffected[0] > 0);
-    })
-    .catch((error) => {
-      response.status(400).send(error?.originalError?.info?.message);
-    })
+app.put("/UpdateProche", async (request, response) => {
+  await UpdateProche(request.body)
+    .then((res) => response.status(200).send(res))
+    .catch((error) => response.status(400).send(error))
 });
-
-app.delete("/DeleteProche/:ProcheId", (request, response) => {
-  new sql.Request()
-    .input("ProcheId", sql.UniqueIdentifier, request.params.ProcheId)
-    .execute('ps_delete_proche')
-    .then((result) => {
-      response.status(200).send(result.rowsAffected[0] > 0);
-    })
-    .catch((error) => {
-      // console.log(error)
-      response.status(400).send(error?.originalError?.info?.message);
-    })
+app.delete("/DeleteProche/:ProcheId", async (request, response) => {
+  await DeleteClient(request.params.ProcheId)
+    .then((res) => response.status(200).send(res))
+    .catch((error) => response.status(400).send(error))
 });
 //#endregion Proche
 
