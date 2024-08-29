7a6c01201483d8afe3b30134583e88db3c5040fc
aminenetwaciila <87432955+aminenetwaciila@users.noreply.github.com>
2024-08-29 19:06:01 +0100

    Update db.js for production

--- Helper/db.js
+++ Helper/db.js
@@ -1,6 +1,7 @@
 const sql = require("mssql");
 
 // dev
+/*
 const config = {
   server: "SQL6032.site4now.net",
   user: "db_a3973b_acm_admin",
@@ -8,15 +9,15 @@ const config = {
   database: "db_a3973b_acm",
   options: { encrypt: false },
 };
-
+*/
 // prod
-// const config = {
-//   server: "SQL6032.site4now.net",
-//   user: "db_a3973b_acmprod_admin", //prod:db_a3973b_acmprod_admin  dev:db_a3973b_acm_admin
-//   password: "acmprod123456", //prod:acm123456  dev:acmprod123456
-//   database: "db_a3973b_acmprod", //prod:db_a3973b_acmprod  dev:db_a3973b_acm
-//   options: { encrypt: false },
-// };
+ const config = {
+   server: "SQL6032.site4now.net",
+   user: "db_a3973b_acmprod_admin", //prod:db_a3973b_acmprod_admin  dev:db_a3973b_acm_admin
+   password: "acmprod123456", //prod:acm123456  dev:acmprod123456
+   database: "db_a3973b_acmprod", //prod:db_a3973b_acmprod  dev:db_a3973b_acm
+   options: { encrypt: false },
+ };
 
 const connect = async () => {
   try {
